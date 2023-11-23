48d71267d79a6492d2a810f69c96953ed9c65d85
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-11-23 06:09:09 -0600

    heroku fix maybe, looked at andrew mell's repo

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// const hbs = exphbs.create();
+const hbs = exphbs.create({
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  helpers: { withAuth: require('./auth')}
+});
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -20,17 +26,17 @@ const sess = {
 };
 
 app.use(session(sess));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
-
+// Set the template engine to render views from the views directory with our routes defined in /controllers.
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
